f948c9072f37714845f79abb7e08b812894c6aed
Misty94 <mms.strickland@gmail.com>
2022-07-06 20:40:38 -0400

    bankAccount

---
+++ fundamentals/oop/Strickland_Misty_BankAccount/bank_account.py
@@ -0,0 +1,47 @@
+# class BankAccount:
+#     all_accounts = []
+
+#     @classmethod
+#     def print_all_accounts(cls):
+#         for account in cls.all_accounts:
+#             print()
+
+    # don't forget to add some default values for these parameters!
+    def __init__(self, int_rate, balance): 
+        # your code here! (remember, instance attributes go here)
+        self.int_rate = int_rate
+        self.balance = balance
+
+    def deposit(self, amount):
+        self.balance += amount
+        return self
+
+    def withdraw(self, amount):
+        if self.balance > amount:
+            self.balance -= amount
+        else:
+            print("Insufficient funds: Charging a $5 fee")
+            self.balance -= 5
+        return self
+
+    def display_account_info(self):
+        print(f"Balance: {self.balance}")
+        return self
+
+    def yield_interest(self):
+        if self.balance > 0:
+            self.balance += (self.balance * self.int_rate)
+        return self
+
+
+
+account_1 = BankAccount(0.01, 987.63)
+
+account_1.deposit(205).deposit(57.29).deposit(413).withdraw(546.82).yield_interest().display_account_info()
+
+
+account_2 = BankAccount(0.05, 654.17)
+
+account_2.deposit(497.52).deposit(180).withdraw(27.99).withdraw(152.32).withdraw(90.25).withdraw(279.64).yield_interest().display_account_info()
+
+
--- fundamentals/oop/week1wed/main.py
+++ fundamentals/oop/week1wed/main.py
@@ -1,3 +1,9 @@
+import random
+
+#get a rounded integer
+random_int = random.random_int(1, 100)
+print(random_int)
+
 
 # a Method is a function inside a class
 # Class is like the blueprint
@@ -18,24 +24,54 @@ class Student: # begin with an uppercase letter
         print ( f"Instructor: {self.instructor}")
         print ( f"Course: {self.course}")
 
+    def enroll(self,course):
+        self.course = course
+        instructor = random.choice(course.instructors)
+        self.instructor = instructor
+        return self
+
 class Course:
+    # next line is a class attribute - to keep track of 
+    all_courses = []
+
+    @classmethod
+    def print_all_courses(cls):
+        for course in cls.all_courses:
+            print(course)
+
+
     def __init__( self, data ):
         self.name = data["name"]
         self.instructors = data["instructors"]
         self.program = data["program"]
+        Course.all_courses.append(self)
 
     def print_instructor_list( self ):
         for instructor in self.instructors:
             print( instructor)
+        #return self to enable chaining
+        return self
 
     def update_instructor( self, new_name, index ):
         if index < len( self.instructors ):
             self.instructors[index] = new_name
+        return self
     
     def print_info( self ):
         print( f"Program: {self.program}")
         print( f"Name: {self.name}")
         self.print_instructor_list()
+    return self
+
+    @staticmethod
+    def determine_eligibility(age):
+        if age >= 18:
+            return True
+        return False
+
+# for debugging = string representation of our instance
+    def __repr__(self):
+        return f"{self.name} is a {self.program} program" 
 
 python = {
     "name": "Python/Flask",
@@ -43,18 +79,49 @@ python = {
     "program": "Full stack"
 }
 
-course_python = Course( python )
+java = {
+    "name": "Java",
+    "instructors": ["Alfredo Salazar", "Spencer Rauch", "Tyler Tybault"],
+    "program": "Full stack"
+}
 
-course_python.print_info()
-course_python.update_instructor("Ryan Mendez", 2)
-course_python.print_info()
+mern = {
+    "name": "MERN",
+    "instructors": ["Alfredo Salazar", "Spencer Rauch", "Tyler Tybault"],
+    "program": "Full stack"
+}
+
+course_python = Course( python )
+course_python = Course( java )
+course_python = Course( mern )
+
+Course.print_all_courses()
+print(Course.all_courses)
+
+# Chaining
+course_python.print_info().update_instructor("Ryan Mendez", 2).print_info()
+
+# course_python.print_info()
+# course_python.update_instructor("Ryan Mendez", 2)
+# course_python.print_info()
 
 # student_alex = __init__( )
 
 
 # Creating an object / Making an instance of the Student class
 student_alex = Student( "Alex", "Miller", 20, "Nichole", "Web Fundamentals" ) # now student_alex is an object
-student_martha = Student( "Martha", "Smith", 25, "Spencer", "Python")
+# student_martha = Student( "Martha", "Smith", 25, "Spencer", "Python")
+student_martha = Student( "Martha", "Smith", 25, "Spencer", course_python)
+
+if Course.determine_eligibility(student_alex.age):
+    student_alex.enroll(course_java)
+    print("Welcome to Java!")
+else:
+    print("I'm sorry, you're not quite old enough")
+
+student_alex_print_info("After enrollment")
+
+student_martha.course.print_instructor_list()
 
 list_students = []
 list_students.append( student_alex )
@@ -69,3 +136,5 @@ student_alex.print_info("Hey there!")
 student_martha.print_info( "Howdy!" )
 
 print(student_alex.first_name, student_alex.last_name, student_alex.age)
+
+student_martha.print_info("*****************").enroll.print_info(course_python).print_info("****After Enrollment*****")

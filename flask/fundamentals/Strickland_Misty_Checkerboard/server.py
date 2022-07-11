a50e0448a4151f8f79bfa968268bdeac807e37da
Misty94 <mms.strickland@gmail.com>
2022-07-11 15:11:57 -0400

    checkerboard colors

--- flask/fundamentals/Strickland_Misty_Checkerboard/server.py
+++ flask/fundamentals/Strickland_Misty_Checkerboard/server.py
@@ -2,36 +2,36 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
+# def checkerboard1():
+#     num_of_rows = 8
+#     col = 8
+
+#     return render_template("index.html", times=num_of_rows, col=col)
+
+# def small_checkerboard(x):
+#     row = int(x)
+#     col = 8
+
+
+#     return render_template("index.html", times=row, col=col)
+
 @app.route('/')
-def checkerboard1():
-    num_of_rows = 8
-    col = 8
-
-    return render_template("index.html", times=num_of_rows, col=col)
-
 @app.route('/<x>')
-def small_checkerboard(x):
-    row = int(x)
-    col = 8
-
-
-    return render_template("index.html", times=row, col=col)
-
 @app.route('/<x>/<y>')
-def row_and_col(x, y):
+def row_and_col(x=8, y=8):
     row = int(x)
     col = int(y)
 
     return render_template("index.html", times=row, col=col)
 
-# @app.route('/<x>/<y>/<color1>/<color2>')
-# def colors(x, y, color1, color2):
-#     row = int(x)
-#     col = int(y)
-#     color1 = color1
-#     color2 = color2
+@app.route('/<x>/<y>/<color1>/<color2>')
+def colors(x, y, color1, color2):
+    row = int(x)
+    col = int(y)
+    color1 = color1
+    color2 = color2
 
-#     return render_template("index.html", times=row, col=col, color1=color1, color2=color2)
+    return render_template("index.html", times=row, col=col, color1=color1, color2=color2)
 
 
 

639e6eaa6894b82eb27d25d2bd22cc2bb9a37aac
Misty94 <mms.strickland@gmail.com>
2022-07-11 22:42:34 -0400

    counter

--- flask/fundamentals/Strickland_Misty_Counter/server.py
+++ flask/fundamentals/Strickland_Misty_Counter/server.py
@@ -1,12 +1,27 @@
-from flask import Flask
+from flask import Flask, render_template, redirect, session
 app = Flask(__name__)
-# app.secret_key = "It is my secret"
+app.secret_key = "I have a secret"
+
 
 
 @app.route('/')
-def index():
+def counter():
 
-    return "hello"
+
+    if 'counts' in session:
+        session['counts'] += 1
+        print('key exists!')
+    else:
+        session['counts'] = 1
+        print("key 'counts' does NOT exist")
+
+    return render_template("index.html")
+
+@app.route('/destroy_session')
+def destroy():
+    session.clear()		# clears all keys
+
+    return redirect('/')
 
 
 if __name__ == "__main__":
